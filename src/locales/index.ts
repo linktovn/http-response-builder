2602d3742cb38c2300fa027d42bf0e6ffe95fe32
steven <admin@linkto.vn>
2025-05-16 10:33:35 +0700

    Complete locales messages response

---
+++ src/locales/index.ts
@@ -0,0 +1,12 @@
+// src/locales/index.ts
+import enMessages from './en/response-messages.json';
+import viMessages from './vi/response-messages.json';
+import koMessages from './ko/response-messages.json';
+
+export const responseMessages = {
+  en: enMessages,
+  vi: viMessages,
+  ko: koMessages
+};
+
+export default responseMessages;
